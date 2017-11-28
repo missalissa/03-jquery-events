45337970fde300165bafd2e84bd0023b278e61b8
Richard Livingston <livingston.richard@gmail.com>
2017-11-27 17:03:43 -0800

    got the form selection to populate only the selected authors articles

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -45,15 +45,15 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
     $('#author-filter').on('change', function() {
-        // const selectedAuthorName = $(this).attr('data-author');
+        const selectedAuthorName = $(this).attr('data-author');
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
         if ($(this).val()) {
             // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
             // Use an "attribute selector" to find those articles, and fade them in for the reader.
             $('article').hide();
-            
-            // $(`section#${author-name}`).show();
 
+
+            $(`article[data-author='${$(this).val()}']`).fadeIn();
         } else {
             // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
 
